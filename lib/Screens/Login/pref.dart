1bcf079b058415b62623ea72c2e3019a6d5adc7e
Dominique Youness <youness.dominique@gmail.com>
2022-10-05 15:35:34 +0200

    [FIX] Update package login

--- lib/Screens/Login/pref.dart
+++ lib/Screens/Login/pref.dart
@@ -1,25 +1,6 @@
-/*
- *  This file is part of BlackHole (https://github.com/Sangwan5688/BlackHole).
- * 
- * BlackHole is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * BlackHole is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with BlackHole.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Copyright (c) 2021-2022, Ankit Sangwan
- */
-
-import 'package:blackhole/CustomWidgets/gradient_containers.dart';
-import 'package:blackhole/CustomWidgets/snackbar.dart';
-// import 'package:blackhole/Helpers/countrycodes.dart';
+import 'package:imbisha/CustomWidgets/gradient_containers.dart';
+import 'package:imbisha/CustomWidgets/snackbar.dart';
+// import 'package:imbisha/Helpers/countrycodes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hive/hive.dart';
@@ -33,22 +14,8 @@ class PrefScreen extends StatefulWidget {
 
 class _PrefScreenState extends State<PrefScreen> {
   List<String> languages = [
-    'Hindi',
+    'Français',
     'English',
-    'Punjabi',
-    'Tamil',
-    'Telugu',
-    'Marathi',
-    'Gujarati',
-    'Bengali',
-    'Kannada',
-    'Bhojpuri',
-    'Malayalam',
-    'Urdu',
-    'Haryanvi',
-    'Rajasthani',
-    'Odia',
-    'Assamese'
   ];
   List<bool> isSelected = [true, false];
   List preferredLanguage = Hive.box('settings')
@@ -277,7 +244,7 @@ class _PrefScreenState extends State<PrefScreen> {
                                                       TextButton(
                                                         style: TextButton
                                                             .styleFrom(
-                                                          foregroundColor:
+                                                          primary:
                                                               Theme.of(context)
                                                                   .colorScheme
                                                                   .secondary,
@@ -297,7 +264,7 @@ class _PrefScreenState extends State<PrefScreen> {
                                                       TextButton(
                                                         style: TextButton
                                                             .styleFrom(
-                                                          foregroundColor:
+                                                          primary:
                                                               Theme.of(context)
                                                                   .colorScheme
                                                                   .secondary,

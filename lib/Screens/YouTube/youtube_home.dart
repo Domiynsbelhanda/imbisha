37d4988a2fb6c1435f6231d2031315f97c1c2856
Dominique Youness <youness.dominique@gmail.com>
2022-10-05 15:28:14 +0200

    [FIX] YouTube screen

--- lib/Screens/YouTube/youtube_home.dart
+++ lib/Screens/YouTube/youtube_home.dart
@@ -1,27 +1,8 @@
-/*
- *  This file is part of BlackHole (https://github.com/Sangwan5688/BlackHole).
- * 
- * BlackHole is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * BlackHole is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with BlackHole.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Copyright (c) 2021-2022, Ankit Sangwan
- */
-
-import 'package:blackhole/CustomWidgets/on_hover.dart';
-import 'package:blackhole/CustomWidgets/search_bar.dart';
-import 'package:blackhole/Screens/YouTube/youtube_playlist.dart';
-import 'package:blackhole/Screens/YouTube/youtube_search.dart';
-import 'package:blackhole/Services/youtube_services.dart';
+import 'package:imbisha/CustomWidgets/on_hover.dart';
+import 'package:imbisha/CustomWidgets/search_bar.dart';
+import 'package:imbisha/Screens/YouTube/youtube_playlist.dart';
+import 'package:imbisha/Screens/YouTube/youtube_search.dart';
+import 'package:imbisha/Services/youtube_services.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';

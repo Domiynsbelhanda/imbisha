c0a3c5e797a23dcae9c678e01d67450419ea49b2
Dominique Youness <youness.dominique@gmail.com>
2022-10-05 15:56:30 +0200

    [FIX] Android configuration and Supabase

--- lib/Helpers/supabase.dart
+++ lib/Helpers/supabase.dart
@@ -1,28 +1,9 @@
-/*
- *  This file is part of BlackHole (https://github.com/Sangwan5688/BlackHole).
- * 
- * BlackHole is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * BlackHole is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with BlackHole.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Copyright (c) 2021-2022, Ankit Sangwan
- */
-
 import 'package:supabase/supabase.dart';
 
 class SupaBase {
   final SupabaseClient client = SupabaseClient(
-    'https://vuakihfddljlzovzbdaf.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzU1MTk3MywiZXhwIjoxOTQzMTI3OTczfQ.4PzxpfIk81ZvLtUOe0muHVGiZLr-dMK7BLyFsUcrVtc',
+    'https://oexssbhsqxsnailmuamp.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9leHNzYmhzcXhzbmFpbG11YW1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5Nzc2NTksImV4cCI6MTk4MDU1MzY1OX0.smuxSYHQkUk85gJqR7OHkE5lEMRz-E2r8vlyenHde5c',
   );
 
   Future<Map> getUpdate() async {
